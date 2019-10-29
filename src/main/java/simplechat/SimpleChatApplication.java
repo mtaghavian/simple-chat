f78b68eaaa0df27abebbb7bdae1d7322baad2ca1
mtaghavian <masoud.taghavian@gmail.com>
2019-10-29 16:38:27 +0330

    init

--- src/main/java/simplechat/SimpleChatApplication.java
+++ src/main/java/simplechat/SimpleChatApplication.java
@@ -15,9 +15,6 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import simplechat.config.HttpInterceptor;
-import simplechat.model.User;
-import simplechat.repository.UserRepository;
-import simplechat.util.ByteUtils;
 
 import javax.annotation.PostConstruct;
 
@@ -26,58 +23,10 @@ import javax.annotation.PostConstruct;
 @EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
 public class SimpleChatApplication implements ApplicationContextAware {
 
-    public static final String pageResourcePath = "res/html";
-    public static final String styleResourcePath = "res/css";
-    public static final String scriptResourcePath = "res/js";
-    public static final String imageResourcePath = "res/img";
-    public static final String miscResourcePath = "res/misc";
-    public static final String adminUsername = "admin";
-    public static final String broadcastUsername = "broadcast";
-    public static final String imgThumbnailFormat = "jpg";
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private ByteUtils byteUtils;
-
     private static ApplicationContext context;
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleChatApplication.class, args);
-    }
-
-    @PostConstruct
-    public void starter() {
-        if (userRepository.findByUsername("admin") == null) {
-            User admin = new User();
-            admin.setUsername(adminUsername);
-            admin.setPassword(byteUtils.hash("admin"));
-            admin.setFirstname("Masoud");
-            admin.setLastname("Taghavian");
-            userRepository.save(admin);
-
-            User bcUSer = new User();
-            bcUSer.setUsername(broadcastUsername);
-            bcUSer.setPassword(byteUtils.hash("123456"));
-            bcUSer.setFirstname("Broadcast");
-            bcUSer.setLastname("");
-            userRepository.save(bcUSer);
-
-            User u1 = new User();
-            u1.setUsername("me");
-            u1.setPassword(byteUtils.hash("123456"));
-            u1.setFirstname("Mohsen");
-            u1.setLastname("Esmaeili");
-            userRepository.save(u1);
-
-            User u2 = new User();
-            u2.setUsername("sz");
-            u2.setPassword(byteUtils.hash("123456"));
-            u2.setFirstname("Saeed");
-            u2.setLastname("Zhiany");
-            userRepository.save(u2);
-        }
     }
 
     @Override
@@ -103,12 +52,5 @@ public class SimpleChatApplication implements ApplicationContextAware {
         public void addInterceptors(InterceptorRegistry registry) {
             registry.addInterceptor(serviceInterceptor);
         }
-    }
-
-    @Bean(name = "multipartResolver")
-    public CommonsMultipartResolver multipartResolver() {
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-        multipartResolver.setMaxUploadSize(10000000000l);
-        return multipartResolver;
     }
 }
--- src/main/java/simplechat/config/HttpInterceptor.java
+++ src/main/java/simplechat/config/HttpInterceptor.java
@@ -3,227 +3,48 @@ package simplechat.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import simplechat.SimpleChatApplication;
-import simplechat.model.FileInfo;
-import simplechat.model.Session;
-import simplechat.model.User;
-import simplechat.repository.FileDataRepository;
-import simplechat.repository.FileInfoRepository;
-import simplechat.repository.SessionRepository;
-import simplechat.repository.UserRepository;
-import simplechat.util.ByteUtils;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.File;
-import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 @Component
 public class HttpInterceptor implements HandlerInterceptor {
 
-    @Autowired
-    private SessionRepository sessionRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private ByteUtils byteUtils;
-
-    @Autowired
-    private FileInfoRepository fileInfoRepository;
-
-    @Autowired
-    private FileDataRepository fileDataRepository;
-
-    private Set<String> allowedUrls = new HashSet<>();
-    private Set<String> allowedFiles = new HashSet<>();
-    private Set<String> htmlFiles = new HashSet<>();
-
     public HttpInterceptor() {
-        allowedUrls.add("/");
-        allowedUrls.add("/home");
-        allowedUrls.add("/index");
-        allowedUrls.add("/login-helper");
-        allowedUrls.add("/signup");
-        allowedUrls.add("/signup-helper");
-
-        for (String filename : Objects.requireNonNull(new File(SimpleChatApplication.imageResourcePath).list())) {
-            allowedFiles.add("/" + filename);
-        }
-        for (String filename : Objects.requireNonNull(new File(SimpleChatApplication.styleResourcePath).list())) {
-            allowedFiles.add("/" + filename);
-        }
-        for (String filename : Objects.requireNonNull(new File(SimpleChatApplication.scriptResourcePath).list())) {
-            allowedFiles.add("/" + filename);
-        }
-        for (String filename : Objects.requireNonNull(new File(SimpleChatApplication.miscResourcePath).list())) {
-            allowedFiles.add("/" + filename);
-        }
-        for (String filename : Objects.requireNonNull(new File(SimpleChatApplication.pageResourcePath).list())) {
-            htmlFiles.add("/" + filename);
-        }
     }
 
-    private void putHeaderParams(Map<String, String> params, User user) {
-        if (user != null) {
-            params.put("headerAccountText", user.getFirstname() + " " + user.getLastname());
-            params.put("headerAccountAction", "");
-            params.put("headerAccountClasses", "Dropdown");
-            params.put("HeaderUserCaretSvgDisplay", "inline-block");
-        } else {
-            params.put("headerAccountText", "Sign In / Sign Up");
-            params.put("headerAccountAction", "onclick='location.href=\"/user\"'");
-            params.put("headerAccountClasses", "");
-            params.put("HeaderUserCaretSvgDisplay", "none");
-        }
+    @Scheduled(fixedRate = 300000, initialDelay = 300000)
+    public void pingGoogleToKeepServiceAlive() {
+        ping("https://mtaghavian-simplechat.herokuapp.com/");
     }
 
-    private Properties getPropertiesFromCookies(Cookie cookie[]) {
-        if (cookie == null) {
-            return null;
+    public void ping(String uriStr){
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            URI uri = new URI(uriStr);
+            restTemplate.getForEntity(uri, String.class);
+        } catch (URISyntaxException ex) {
+            ex.printStackTrace();
         }
-        Properties cookies = new Properties();
-        for (Cookie c : cookie) {
-            cookies.put(c.getName(), c.getValue());
-        }
-        return cookies;
-    }
-
-    public void loginWithCookies(Properties cookies, Session session) {
-        if (cookies != null) {
-            if (cookies.containsKey("username") && cookies.containsKey("password")) {
-                User dbUser = userRepository.findByUsername("" + cookies.get("username"));
-                if ((dbUser != null) && dbUser.getPassword().equals(cookies.get("password"))) {
-                    session.setUser(dbUser);
-                }
-            }
-        }
-    }
-
-    private void loginWithBasicAuth(HttpServletRequest request, HttpSession httpSession, Session session) throws UnsupportedEncodingException {
-        String auth = request.getHeader("Authorization");
-        if (auth != null) {
-            String[] split = auth.split(" ");
-            if ("basic".equals(split[0].toLowerCase())) {
-                String base64 = split[1];
-                String cred = new String(Base64.getDecoder().decode(base64), "UTF-8");
-                String username = cred.substring(0, cred.indexOf(":"));
-                String password = cred.substring(cred.indexOf(":") + 1, cred.length());
-                User dbUser = userRepository.findByUsername(username);
-                if ((dbUser != null) && dbUser.getPassword().equals(byteUtils.hash(password))) {
-                    session.setUser(dbUser);
-                }
-            }
-        }
-    }
-
-    @Scheduled(fixedRate = 3600000, initialDelay = 3600000)
-    public void clearExpiredSessions() {
-        List<Session> expired = sessionRepository.findExpired(System.currentTimeMillis() - 3600000);
-        sessionRepository.deleteAll(expired);
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String uri = request.getRequestURI();
         HttpSession httpSession = request.getSession();
-
-        Session session = sessionRepository.findById(httpSession.getId()).orElse(null);
-        if (session == null) {
-            session = new Session(httpSession.getId(), null, System.currentTimeMillis());
-        }
-        session.setLastModified(System.currentTimeMillis());
-        sessionRepository.save(session);
-
-        loginWithBasicAuth(request, httpSession, session);
-        loginWithCookies(getPropertiesFromCookies(request.getCookies()), session);
-
-        if (allowedFiles.contains(uri)) {
-            String uriLC = uri.toLowerCase();
-            if (uriLC.endsWith(".svg")) {
-                response.setContentType("image/svg+xml");
-                response.getOutputStream().write(byteUtils.readBytes(new File((SimpleChatApplication.imageResourcePath + "/" + uri)), false));
-            } else if (uriLC.endsWith(".css")) {
-                response.setContentType("text/css");
-                response.getOutputStream().write(byteUtils.readBytes(new File((SimpleChatApplication.styleResourcePath + "/" + uri)), false));
-            } else if (uriLC.endsWith(".json")) {
-                response.setContentType("application/json");
-                response.getOutputStream().write(byteUtils.readBytes(new File((SimpleChatApplication.miscResourcePath + "/" + uri)), false));
-            } else if (uriLC.endsWith(".js")) {
-                response.setContentType("text/javascript");
-                response.getOutputStream().write(byteUtils.readBytes(new File((SimpleChatApplication.scriptResourcePath + "/" + uri)), false));
-            } else if (uriLC.endsWith(".png")) {
-                response.setContentType("image/png");
-                response.getOutputStream().write(byteUtils.readBytes(new File((SimpleChatApplication.imageResourcePath + "/" + uri)), false));
-            } else if (uriLC.endsWith(".ico")) {
-                response.setContentType("image/x-icon");
-                response.getOutputStream().write(byteUtils.readBytes(new File((SimpleChatApplication.imageResourcePath + "/" + uri)), false));
-            }
-            response.setStatus(HttpServletResponse.SC_OK);
-            return false;
-        }
-
-        if (uri.startsWith("/image-file-preview/")) {
-            String fileInfoId = uri.substring(uri.indexOf('/', 1) + 1);
-            FileInfo info = fileInfoRepository.findById(UUID.fromString(fileInfoId)).get();
-            MediaType mediaType;
-            byte fileData[];
-            if (info.getImgPrevFileDataId() == null) {
-                mediaType = byteUtils.getMediaType(info.getName());
-                fileData = fileDataRepository.findById(info.getFileDataId()).get().getData();
-            } else {
-                mediaType = byteUtils.getMediaType("a." + SimpleChatApplication.imgThumbnailFormat);
-                fileData = fileDataRepository.findById(info.getImgPrevFileDataId()).get().getData();
-            }
-            response.setContentType("" + mediaType);
-            response.getOutputStream().write(fileData);
-            return false;
-        }
-
-        if (!allowedUrls.contains(uri)) {
-            if ("/login".equals(uri)) {
-                if (session.getUser() != null) {
-                    response.sendRedirect("/user");
-                    return false;
-                }
-            } else {
-                if (session.getUser() == null) {
-                    session.setRedirectedUri(uri);
-                    response.sendRedirect("/login");
-                    return false;
-                }
-            }
-        }
-
-        if (uri.equals("/")) {
-            uri = "/home";
-        }
-        if (htmlFiles.contains(uri + ".html")) {
-            Map<String, String> params = new HashMap<>();
-            putHeaderParams(params, session.getUser());
-            params.put("pageTitle", uri.substring(1));
-            if (uri.equals("/user")) {
-                params.put("optionsSvg",
-                        "<img src=\"options.svg\" " +
-                                "id=\"OptionsSvg\" " +
-                                "class=\"Button ButtonTransPrimary\"" +
-                                "onclick='toggleSidebarDisplay()'/>");
-            } else {
-                params.put("optionsSvg", "");
-            }
-            String html = byteUtils.readPage(uri + ".html", params);
-            response.setContentType("text/html");
-            response.getOutputStream().write(html.getBytes("UTF-8"));
-            return false;
-        }
-        return true;
+        response.setContentType("text/html");
+        response.getOutputStream().write("This is a test".getBytes());
+        return false;
     }
 
     @Override
